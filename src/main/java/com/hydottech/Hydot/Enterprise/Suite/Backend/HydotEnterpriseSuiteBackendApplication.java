b9719fd0e11902fe57454a42369e2f7223616f2f
Solomon <solomondanso2023@gmail.com>
2024-09-29 20:05:28 +0000

    Project Setup

---
+++ src/main/java/com/hydottech/Hydot/Enterprise/Suite/Backend/HydotEnterpriseSuiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hydottech.Hydot.Enterprise.Suite.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydotEnterpriseSuiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydotEnterpriseSuiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hydottech/Hydot/Enterprise/Suite/Backend/HydotEnterpriseSuiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydottech.Hydot.Enterprise.Suite.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydotEnterpriseSuiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
